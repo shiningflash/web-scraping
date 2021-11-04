c20d453105331d14d39e0f66e8ddabda9d09f05e
Amirul Islam <amirulislamalmamun@gmail.com>
2021-11-05 01:47:22 +0600

    timesjobs website scraping added

--- sample/main.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open('home.html', 'r') as html_file:
-    content = html_file.read()
-    
-    soup = BeautifulSoup(content, 'lxml')
-    print(soup.prettify())
---
+++ timesjobs/main.py
@@ -0,0 +1,44 @@
+import json
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def save_response(data, filename):
+    with open(filename, 'w') as f:
+        f.write(json.dumps(data, indent=4))
+    print(f'Response saved in {filename}')
+
+
+def find_jobs(my_skill):
+    suitable_jobs = []
+    
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    
+    for job in jobs:
+        published_date = ' '.join(job.find('span', class_='sim-posted').span.text.split())
+        if not "few" in published_date.lower():
+            continue
+        skills = ' '.join(job.find('span', class_='srp-skills').text.split()).replace(' ,', ',')
+        if my_skill.strip().lower() not in skills.lower():
+            continue
+        company_name = ' '.join(job.find('h3', class_='joblist-comp-name').text.split())
+        more_info = job.header.h2.a['href']
+
+        suitable_jobs.append({
+            'Company Name': company_name,
+            'Required Skills': skills,
+            'More info': more_info
+        })
+    save_response(suitable_jobs, 'jobs.json')
+
+
+if __name__ == '__main__':
+    print('Search with your skill')
+    my_skill = input('>')
+    print(f'Filtering out {my_skill}...\n')
+    
+    find_jobs(my_skill)
+    
